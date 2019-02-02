0dde7a4ed8581afec919e2ee6468aeabdc69a511
Nishank Sanjeev Samant <nishanksamant@gmail.com>
2019-02-02 21:00:20 +1100

    Added UI and React Routes functionality

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Form = props => (
-   <form onSubmit={props.getRecipe}>
-        <input type="text" name="recipe_name"/>
-        <button>Search</button>
+   <form onSubmit={props.getRecipe} style={{ marginBottom:"2rem" }}>
+        <input className="form__input" type="text" name="recipe_name"/>
+        <button className="form__button">Search</button>
    </form>
 );
 
